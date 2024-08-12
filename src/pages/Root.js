c7c9e376b66ceef2975d3f904e34f20fae8bc70a
SolarOwl0113 <157780735+SolarOwl0113@users.noreply.github.com>
2024-08-12 09:23:03 +0900

    Root.js에 조건부 랜더링 추가 및 Root.css수정 (#11)
    Co-authored-by: SS Lee <edian113@naver.com>

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { useLocation, Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import './Root.css';
 
 export default function RootLayout(){
 
-    return(
-      <div className="app">
-        <Header />
-        <Outlet />
-        {/* <Footer/> */}
-      </div>
+  const location = useLocation();
+  const isIndexPage = location.pathname === '/';
+
+  return (
+    <div className="app">
+      {!isIndexPage && <Header />}
+      <Outlet />
+      {!isIndexPage && <Footer />}
+    </div>
     )
   }
