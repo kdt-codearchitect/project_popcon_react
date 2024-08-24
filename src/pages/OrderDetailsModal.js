7e05a0f8ee60560009fbb369679b5c1230a827e5
gi-yun <141106440+gi-yun@users.noreply.github.com>
2024-08-24 18:05:11 +0900

    Feature/pop64 (#74)
    * add OrderHistory
    
    * fix image#3

--- src/pages/OrderDetailsModal.js
+++ src/pages/OrderDetailsModal.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const OrderDetailsModal = ({ order, orderItems, closeModal }) => {
+
     const imgSrc = '../image/item_image/';
+
   return (
     <div className="modal">
       <div className="modal-content">
@@ -12,23 +14,28 @@ const OrderDetailsModal = ({ order, orderItems, closeModal }) => {
         <p>Order Status: {order.orderStatus || 'N/A'}</p>
         <p>Delivery Status: {order.deliveryStatus || 'N/A'}</p>
 
+
         <h3>Order Items</h3>
         <ul>
           
           
           {orderItems.map((item, index) => (
             <li key={index}>
-              <img src={imgSrc + item.skuImg} alt={order.skuName} />  이름: {item.skuName}, 수량: {item.orderItemQty}, 가격: {item.orderItemPrice.toLocaleString()}원
+
+              <img src={imgSrc + orderItems.skuName} alt={orderItems.skuName} />  이름: {item.skuName}, 수량: {item.orderItemQty}, 가격: {item.orderItemPrice.toLocaleString()}원
+
             </li>
           ))}
         
 
             <li >
+
                결제금액 :{order.totalSumPrice ? order.totalSumPrice.toLocaleString() + '원' : 'N/A'}
             </li>
           
         </ul>
           <button onClick={closeModal}>Close</button>
+
       </div>
     </div>
   );
