680bebc40e9cd50b548420eb5fcc56d857a3ef40
CarrotGold <chunius@naver.com>
2024-08-27 15:11:20 +0900

    POP-53 마이페이지 페이지 디자인 및 링크 구현 완료

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+
 import './MyPage.css';
 import { Link, useNavigate } from "react-router-dom";
 import SideMenu from './SideMenu';
@@ -9,48 +9,10 @@ import fridge from '../image/mypage_fridge.png';
 import profile from '../image/mypage_profile.png';
 
 
-
-
-
 const MyPage = () => {
-  const navigate = useNavigate();
-  const [userList, setUserList] = useState([]);
-  const url = process.env.REACT_APP_API_BASE_URL;
 
-  useEffect(() => {
-    
-    const fetchMembers = async () => {
-      try {
-        const response = await fetch(url+'/MyPage');  
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const json = await response.json();
-        setUserList(json);
-      } catch (error) {
-        console.error("There was an error fetching the members!", error);
-      }
-    };
-
-    fetchMembers();
-  }, []);
-  console.log(localStorage.getItem('customerIdx'));
-  console.log(localStorage.getItem('userid'));
   return (
-    <div className="page-container black-bg">
-      {/* <div className="mypage-container">
-        <div className="mypage-content">
-          <h2 className="mypage-title" onClick={() => navigate('/MyPage')}>마이페이지</h2>
-          <ul className="nav-links-side">
-            <li><Link to="/MyInfo">MyInfo / 개인정보수정</Link></li>
-            <li><Link to="/Wish">Favorites / 나의 찜 목록</Link></li>
-            <li><Link to="/MyDelivery">Delivery / 배송 상황</Link></li>
-            <li><Link to="/refrigerator">Fridge / 나의 냉장고</Link></li>
-            <li><Link to="/Payment">Payment / 결제수단</Link></li>
-            <li><Link to="/orderhistory">History / 주문 내역</Link></li>
-          </ul>
-        </div>
-      </div> */}
+    <div className="page-container">
       <SideMenu/>
       <div className="section-container">
         <div className="section-header">
@@ -59,22 +21,22 @@ const MyPage = () => {
           </div>
         </div>
         <div className="section-content">
-          <div className="">
-            
-          <div>
-            <img src={profile} className='mypage-icon'/>
-            <img src={cart} className='mypage-icon'/>
-            <img src={delivery} className='mypage-icon'/>
-            <img src={fridge} className='mypage-icon'/>
-            <img src={checkouts} className='mypage-icon'/>
-          </div>
-            <ul>
-              {userList.map(member => (
-                <li key={member.id}>
-                  {member.name} ({member.email})
-                </li>
-              ))}
-            </ul>
+          <div className="flex-sa flex-width">
+            <div className='mypage-text'>
+              <Link to="/MyInfo"><img src={profile} className='mypage-icon'/></Link>
+              <p>나의정보</ p></div>
+            <div className='mypage-text'>
+              <Link to="/Cart"><img src={cart} className='mypage-icon'/></Link>
+              <p>장바구니</p></div>
+            <div className='mypage-text'>
+              <Link to="/Maps"><img src={delivery}  className='mypage-icon'/></Link>
+              <p>배송지정보</p></div>
+            <div className='mypage-text'>
+              <Link to="/refrigerator"><img src={fridge} className='mypage-icon'/></Link>
+              <p>냉장고</p></div>
+            <div className='mypage-text'>
+              <Link to="/orderhistory"><img src={checkouts} className='mypage-icon'/></Link>
+              <p>주문내역</p></div>
           </div>
         </div>
       </div>
