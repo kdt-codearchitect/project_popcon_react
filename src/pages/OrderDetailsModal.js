4c22ad2245518d001aea7a0eeaa6b85a85976994
SolarOwl0113 <157780735+SolarOwl0113@users.noreply.github.com>
2024-08-28 19:21:39 +0900

    fix : MapComponent/OrderDetailsModal 수정 (#108)

--- src/pages/OrderDetailsModal.js
+++ src/pages/OrderDetailsModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './OrderDetailsModal.css';  // CSS 파일을 import
+import { ImCross } from "react-icons/im";
 
 const OrderDetailsModal = ({ order, orderItems, closeModal }) => {
 
@@ -8,23 +9,19 @@ const OrderDetailsModal = ({ order, orderItems, closeModal }) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <span className="close-button" onClick={closeModal}>&times;</span>
-        <h2>Order Details</h2>
-        <p><strong>Order ID:</strong> {order.orderIdx}</p>
-        <p><strong>Payment ID:</strong> {order.paymentId}</p>
-        <p><strong>Order Time:</strong> {order.orderTime ? new Date(order.orderTime).toLocaleString() : 'N/A'}</p>
-        <p><strong>Order Status:</strong> {order.orderStatus || 'N/A'}</p>
-        <p><strong>Delivery Status:</strong> {order.deliveryStatus || 'N/A'}</p>
+        <h2>주문 내역 상세</h2>
+        <p><strong>주문 ID :</strong> {order.paymentId}</p>
+        <p><strong>주문 날짜 :</strong> {order.orderTime ? new Date(order.orderTime).toLocaleString() : 'N/A'}</p>
 
-        <h3>Order Items</h3>
+        <h3>주문 목록</h3>
         <ul className="order-items-list">
           {orderItems.map((item, index) => (
             <li key={index} className="order-item">
               <img className="order-item-image" src={imgSrc + item.skuName + '.jpg'} alt={item.skuName} />
               <div className="order-item-details">
-                <p><strong>이름:</strong> {item.skuName}</p>
-                <p><strong>수량:</strong> {item.orderItemQty}</p>
-                <p><strong>가격:</strong> {item.orderItemPrice.toLocaleString()}원</p>
+                <p><strong>이름 :</strong> {item.skuName}</p>
+                <p><strong>수량 :</strong> {item.orderItemQty}</p>
+                <p><strong>가격 :</strong> {item.orderItemPrice.toLocaleString()}원</p>
               </div>
             </li>
           ))}
@@ -34,7 +31,7 @@ const OrderDetailsModal = ({ order, orderItems, closeModal }) => {
           <strong>결제금액:</strong> {order.totalSumPrice ? order.totalSumPrice.toLocaleString() + '원' : 'N/A'}
         </div>
         
-        <button className="close-button" onClick={closeModal}>Close</button>
+        <ImCross className="close-button" onClick={closeModal}/>
       </div>
     </div>
   );
